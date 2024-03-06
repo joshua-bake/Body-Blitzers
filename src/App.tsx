e2b572eab77de52af885894ac1d9c3397aaee2f0
Joshua Baker <joshua.jbaker12@icloud.com>
2024-03-06 11:31:36 -0500

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Navbar from "./Components/Navbar"
+import Home from "./Components/Home"
 
-function App(){
-  
+function App() {
 
-  return <h1>Hello world</h1>
+
+  return (
+    <Router>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </Router>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./styles/main.scss"
 import App from "./App"
+import.meta.env.VITE_API_KEY
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
